fee7305edcbb5a12782506cd58b545efd128ad7c
Vinay Shenoy <vinay@obvious.in>
2020-04-23 11:13:35 +0530

    Add base gradle plugin module for room instrumentation

---
+++ buildSrc/src/main/kotlin/org/simple/clinic/plugin/gradle/InstrumentRoomPlugin.kt
@@ -0,0 +1,11 @@
+package org.simple.clinic.plugin.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class InstrumentRoomPlugin: Plugin<Project> {
+
+  override fun apply(target: Project) {
+
+  }
+}
